7b82c638e360b2b95235f796afe166c0ecfda3f7
AKM7622 <akash03082006@gmail.com>
2026-03-11 21:46:49 +0530

    Added Authorization

--- backend/models/User.js
+++ backend/models/User.js
@@ -6,35 +6,42 @@ const UserSchema = new mongoose.Schema({
     required: true,
     unique: true
   },
+
   email: {
     type: String,
     required: true,
     unique: true
   },
+
   password: {
     type: String,
     required: true
   },
+
   profilePic: {
     type: String,
     default: ""
   },
+
   bio: {
     type: String,
     default: ""
   },
+
   followers: [
     {
       type: mongoose.Schema.Types.ObjectId,
       ref: "User"
     }
   ],
+
   following: [
     {
       type: mongoose.Schema.Types.ObjectId,
       ref: "User"
     }
   ]
+
 }, { timestamps: true })
 
 module.exports = mongoose.model("User", UserSchema)
--- backend/server.js
+++ backend/server.js
@@ -3,10 +3,16 @@ const mongoose = require("mongoose")
 const cors = require("cors")
 require("dotenv").config()
 
+const authRoutes = require("./routes/authRoutes")
+const userRoutes = require("./routes/userRoutes")
+
 const app = express()
 
 app.use(cors())
 app.use(express.json())
+
+app.use("/api/auth", authRoutes)
+app.use("/api/users", userRoutes)
 
 app.get("/", (req, res) => {
   res.send("API Running")
